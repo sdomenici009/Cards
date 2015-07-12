7d1ea5ab323c298ad2dd997d1026bf8f6d8d7d5d
Stephen Domenici <sdomenici009@gmail.com>
2015-07-11 21:10:19 -0700

    Prototyping hand/card system.

---
+++ Assets/_Scripts/Card.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Card : MonoBehaviour {
+
+	bool selected = false;
+
+	public Transform hand;
+	public Transform selectedCardPanel;
+
+	public Sprite[] foils;
+
+	[SerializeField]
+	private Image foil;
+	
+	void Start () {
+		for(int i=0; i < transform.childCount; i++)
+		{
+			if(transform.GetChild(i).GetComponent<Text>() != null)
+				transform.GetChild(i).GetComponent<Text>().text = Random.Range(0, 9).ToString();
+		}
+
+		foil.sprite = foils[Random.Range(0, foils.Length)];
+	}
+	
+	void Update () {
+
+		if(selected && Input.touches.Length > 0)
+		{
+			transform.localScale = Vector3.one;
+			transform.position = new Vector3(Input.touches[0].position.x, Input.touches[0].position.y, transform.position.z);
+		}
+
+		if(selected && Input.touches.Length == 0)
+		{
+			transform.SetParent(selectedCardPanel);
+			transform.localScale = Vector3.one*2f;
+			transform.localPosition = Vector3.zero;
+		}
+
+		if(!selected)
+		{
+			transform.SetParent(hand);
+			transform.localScale = Vector3.one;
+		}
+	}
+
+	public void OnClick()
+	{
+		selected = !selected;
+	}
+}
---
+++ Assets/_Scripts/Hand.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Hand : MonoBehaviour {
+
+	List<Card> cards = new List<Card>();
+
+	[SerializeField]
+	private GameObject cardPrefab;
+
+	[SerializeField]
+	private GridLayoutGroup gridLayout;
+
+	[SerializeField]
+	private Transform selectedCardPanel;
+	
+	void Start () {
+		for(int i=0; i < 5; i++)
+		{
+			GameObject card = (GameObject)Instantiate(cardPrefab);
+			card.transform.SetParent(transform);
+			card.transform.localScale = Vector3.one;
+			AddCard(card.GetComponent<Card>());
+		}
+	}
+	
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.A))
+		{
+			GameObject card = (GameObject)Instantiate(cardPrefab);
+			card.transform.SetParent(transform);
+			card.transform.localScale = Vector3.one;
+			AddCard(card.GetComponent<Card>());
+		}
+	}
+
+	void AddCard(Card card)
+	{
+		card.selectedCardPanel = selectedCardPanel;
+		card.hand = transform;
+		cards.Add(card);
+			
+		if(cards.Count > 1)
+			gridLayout.spacing = new Vector2((1080 - gridLayout.padding.left - gridLayout.padding.right - cards.Count*gridLayout.cellSize.x)/(cards.Count-1), gridLayout.spacing.y);
+	}
+}
